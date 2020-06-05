318547757401936370cf371c5ed8be0ed227da20
Cristian Ferrier <cristianferrier@outlook.com>
2020-06-05 09:08:29 -0500

    Breadcrumbs

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -58,7 +58,6 @@ class Main extends Component {
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
-                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
                 <Footer />
             </div>
