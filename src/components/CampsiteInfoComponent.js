f8f4d41faed6a1203e7eaad6ea9a04c5d3a06bbf
Cristian Ferrier <63423208+chrisff87@users.noreply.github.com>
2020-06-16 10:42:58 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -21,7 +21,7 @@ function RenderCampsite({campsite}) {
         );
     }
 
-function RenderComments({comments}) {
+    function RenderComments({comments, addComment, campsiteId}) {
         if (comments) {
             return (
                 <div className="col-md-5 m-1">
@@ -34,7 +34,7 @@ function RenderComments({comments}) {
                             -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                             </p>
                         </div>)}
-                    <CommentForm />
+                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
                 </div>
             )
         }
@@ -59,7 +59,11 @@ function CampsiteInfo(props) {
                     </div>
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments
+                            comments={props.comments}
+                            addComment={props.addComment}
+                            campsiteId={props.campsite.id}
+                        />
                     </div>
                 </div>
             );
@@ -91,6 +95,11 @@ class CommentForm extends React.Component {
         this.toggleModal();
     }
 
+    handleSubmit(values) {
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    }
+    
     render() {
         return(
             <React.Fragment>
